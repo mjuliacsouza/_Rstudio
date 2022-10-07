d4beaf348ca8558e7176ea650525147dd808a187
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-10-07 16:30:38 -0300

    basic statistics

--- basic_statistics.R
+++ basic_statistics.R
@@ -23,3 +23,13 @@ IQR(state[['Populacao']])
 quantile(state[['TaxaHomicidio']], p=c(.05,.25,.5,.75,.95))
 boxplot(state[['Populacao']]/1000000, ylab='Populacao (milhao)')
 
+# frequency table
+breaks <- seq(from=min(state[['Populacao']]), 
+              to=max(state[['Populacao']]), length=11)
+pop_freq <- cut(state[['Populacao']], breaks=breaks, 
+                right=TRUE, include.lowest=TRUE)
+table(pop_freq)
+
+#histogram
+hist(state$TaxaHomicidio, freq=FALSE)
+lines(density(state$TaxaHomicidio), lwd=6, col='blue')
