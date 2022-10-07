2596a3409c80ff395767f02ed1fa7c80925c7ee8
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-10-07 16:20:57 -0300

    basic statistics

--- basic_statistics.R
+++ basic_statistics.R
@@ -9,7 +9,12 @@ max_pop = max(state$Populacao)
 estado_max_pop = subset(state, Populacao == max_pop)
 estado_max_pop
 
-# metrics
+# mean and median
 mean(state[['Populacao']])
 mean(state[['Populacao']], trim=0.1)
 median(state[['Populacao']])
+
+# standard deviation
+sd(state[['Populacao']])
+IQR(state[['Populacao']])
+##mad(state[['Populacao']])
